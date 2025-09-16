4911eeca9e1804de617d95407e5fd00174e361e1
Gabriela Alejandra Bustamante Medina <gabustamantemedina@gmail.com>
2025-09-16 11:29:19 -0600

    Added Again The Project
    -Added the movement script to the player.
    -Player can now move and jump
    -Used Scriptable objects for the configuration of the movement (that way is easier to edit, and not touch the code)

---
+++ Assigment_1_Platform/Assets/Scripts/InputController.cs
@@ -0,0 +1,72 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    private GameControls _gameControls;
+    
+    //Player Controls
+    public event Action JumpEvent;
+    public event Action<Vector2> MoveEvent;
+    public event Action<Vector2> MouseLookEvent;
+
+    public event Action AttackEvent;
+    public event Action AttackEventCancelled;
+    
+    private void Awake()
+    {
+        _gameControls = new GameControls();
+    }
+
+    private void OnEnable()
+    {
+        _gameControls.Player.Enable();
+        
+        _gameControls.Player.Move.performed += OnMovePerformed;
+        _gameControls.Player.Move.canceled += OnMoveCancelled;
+        _gameControls.Player.Jump.performed += OnJumpPerformed; //performed a jump call this function
+        _gameControls.Player.Look.performed += OnMouseMove;
+        _gameControls.Player.Look.canceled += OnMouseMoveCancelled;
+        _gameControls.Player.Attack.performed += OnAttackPerformed;
+        _gameControls.Player.Attack.canceled += OnAttackCancelled;
+     
+       
+    }
+    
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(context.ReadValue<Vector2>()); //excellent
+    }
+
+    private void OnMoveCancelled(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(Vector2.zero);
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        JumpEvent?.Invoke();
+        
+    }
+
+    private void OnMouseMove(InputAction.CallbackContext context)
+    {
+       MouseLookEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+    private void OnMouseMoveCancelled(InputAction.CallbackContext context)
+    {
+        MouseLookEvent?.Invoke(Vector2.zero);
+    }
+    private void OnAttackPerformed(InputAction.CallbackContext context)
+    {
+        AttackEvent?.Invoke();
+    }
+    
+    private void OnAttackCancelled(InputAction.CallbackContext context)
+    {
+        AttackEventCancelled?.Invoke();
+    }
+    
+    
+}
---
+++ Assigment_1_Platform/Assets/Scripts/PlayerControllerConfig.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "PlayerControllerConfig", menuName = "Scriptable Objects/PlayerControllerConfig")]
+public class PlayerControllerConfig : ScriptableObject
+{
+    [Header("Movement")] //good usage of headers; makes it a lot clearer
+    [field:SerializeField]public float MovementSpeed { get; private set; }= 6f; //better practice for variables
+    public float groundAcceleration = 10f; //max velocity when a player is on the floor 
+    public float groundDeceleration = 10f;
+   
+    [Header("Jump")]
+    public float jumpHeight = 10f;
+    public float gravity = 9.81f;
+    [field:SerializeField]public float AirAcceleration { get; private set; } = 5f; //max velocity when a player is in the air 
+    
+    [Header("Rotation")]
+    [field:SerializeField]public float CameraBounds { get; private set; } = 30f;
+    public float mouseSensitivity = 100f;
+
+    [Header("Interact")] 
+    public float interactDistance = 1000f;
+    
+
+}
