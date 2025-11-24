d2f7ea4634a3ffff6e760a177a9950aa349649b2
Gabriela Alejandra Bustamante Medina <gabustamantemedina@gmail.com>
2025-11-23 17:40:01 -0700

    Buttons with animation

--- Assigment_1_Platform/Assets/Scripts/UI/ButtonsTest.cs
+++ Assigment_1_Platform/Assets/Scripts/UI/ButtonsTest.cs
@@ -5,18 +5,19 @@ using UnityEngine.EventSystems;
 public class ButtonsTest : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Tweener _tween;
-    [SerializeField] float _duration = 0.2f;
+    [SerializeField] float duration = 0.2f;
+    [SerializeField] private float buttonScale = 2.0f;
     
     public void OnPointerEnter(PointerEventData eventData)
     {
-        _tween= this.transform.DOScale(1.5f, _duration);
+        _tween= this.transform.DOScale(buttonScale, duration);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         _tween?.Kill();// Must kill old tween
         //Previous transform is PERMANENT, not just an animation. so we scale it back by hand
-        this.transform.DOScale(1f, _duration);
+        this.transform.DOScale(1f, duration);
     }
 
     public void OnClick()
@@ -24,7 +25,7 @@ public class ButtonsTest : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         // Create a sequence 
         // We will set it so that the whole duration is 6
         Sequence sequence = DOTween.Sequence();
-        sequence.Append(transform.DORotate(new Vector3(0, 45, 0), _duration));
+        sequence.Append(transform.DORotate(new Vector3(0, 45, 0), duration));
         //Add a horizontal relative move tween that will last the whole Sequence's duration 
         sequence.SetLoops(-1, LoopType.Yoyo);
     }
